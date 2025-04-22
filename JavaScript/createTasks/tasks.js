e4eae21c283591b27b4a75d17882039381b57c65
artemwebber1 <chekhonin.artem@gmail.com>
2025-04-22 17:30:41 +0300

    изменено добавление задач, исправлена вёрстка

--- JavaScript/createTasks/tasks.js
+++ JavaScript/createTasks/tasks.js
@@ -1,30 +1,56 @@
-document.querySelector(".BtnCheckmarkSection__addbtn--btn").addEventListener("click" , ()=> 
-{
-    const section = document.querySelector(".tasks")
+const tasksSection = document.querySelector(".tasks")
+const createTaskSection = document.querySelector(".create-task-popup")
+const createTaskInput = createTaskSection.querySelector("input")
+const cancelCreateBtn = createTaskSection.querySelector(".tasks__cont--btnCancel")
+const confirmCreateBtn = createTaskSection.querySelector(".tasks__cont--btnAdd")
 
+const hideCreateSection = () => {
+    createTaskInput.value = ""
+    createTaskSection.style.display = "none"
+}
+
+cancelCreateBtn.addEventListener("click", hideCreateSection)
+
+confirmCreateBtn.addEventListener("click", () => {
+    if (createTaskInput.value == "")
+        return
+    createTask(createTaskInput.value)
+    hideCreateSection()
+})
+
+const addBtn = document.querySelector(".BtnCheckmarkSection__addbtn--btn")
+
+addBtn.addEventListener("click" , ()=> 
+{
+    createTaskSection.style.display = "flex"
+})
+
+const createTask = (text) => {
     //create div container
     const taskBlock = document.createElement("div")
     taskBlock.className = "tasks__cont"
 
     //create textarea
     const input = document.createElement('input');
+    input.value = text
     input.className = "tasks__cont--input"
     input.type = "text"
     input.name = "task"
+    input.readOnly = true;
 
     //create buttonDelete
     const btn = document.createElement("button")
     btn.className = "tasks__cont--btnDelete"
     btn.innerHTML = "&times;"
 
-    section.appendChild(taskBlock)
+    tasksSection.appendChild(taskBlock)
 
     taskBlock.appendChild(input);
 
     taskBlock.appendChild(btn);
 
-    btn.addEventListener("click" , ()=>
+    btn.addEventListener("click" , () =>
     {
         taskBlock.remove();
     })
-})
+}
